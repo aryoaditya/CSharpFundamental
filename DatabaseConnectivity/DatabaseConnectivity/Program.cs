4536a7fe7a2e4820997a973de73cfff5df507cd7
Aryo Aditya <aryoaditya2000@gmail.com>
2023-09-19 14:08:54 +0700

    first commit

---
+++ DatabaseConnectivity/DatabaseConnectivity/Program.cs
@@ -0,0 +1,309 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace DatabaseConnectivity;
+
+public class Program
+{
+    static string connectionString = "Data Source=DESKTOP-98R3UR4;Database = db_hr_dts; Integrated Security=True;Connect Timeout=30;";
+    static SqlConnection conn;
+    private static void Main()
+    {
+        // Mengambil dan mencetak semua record pada tabel region
+        GetAllRegions();
+
+        // Insert row ke tabel region dengan nama "Kepulauan Seribu"
+        InsertRegion("Kepulauan Seribu");
+
+        // Mengambil dan mencetak semua record pada tabel region berdasarkan Id
+        GetRegionById(23);
+
+        // Update row pada tabel region berdasarkan Id, mengirim parameter nama yg ingin diubah
+        UpdateRegion(23, "Kepulauan");
+        GetAllRegions();
+
+        // Delete row pada tabel region berdasarkan Id
+        DeleteRegion(23);
+        GetAllRegions();
+    }
+
+    // GET ALL: Region
+    public static void GetAllRegions()
+    {
+        // Membuat objek koneksi ke database
+        using var connection = new SqlConnection(connectionString);
+
+        // Membuat objek untuk perintah SQL
+        using var command = new SqlCommand();
+
+        // Mengatur koneksi untuk objek perintah SQL
+        command.Connection = connection;
+
+        // Menentukan query SELECT yang akan dijalankan
+        command.CommandText = "SELECT * FROM regions";
+
+        try
+        {
+            // Membuka koneksi ke database
+            connection.Open();
+
+            // Mengeksekusi perintah SQL dan mendapatkan objek reader
+            using var reader = command.ExecuteReader();
+
+            // Memeriksa apakah ada baris data yang ditemukan
+            if (reader.HasRows)
+                while (reader.Read())
+                {
+                    // Mencetak Id dan Name dari baris saat ini
+                    Console.WriteLine("Id: " + reader.GetInt32(0));
+                    Console.WriteLine("Name: " + reader.GetString(1));
+                }
+            else
+                // Jika tidak ada baris data yang ditemukan
+                Console.WriteLine("No rows found.");
+
+            // Menutup objek reader
+            reader.Close();
+
+            // Menutup koneksi ke database
+            connection.Close();
+        }
+        catch (Exception ex)
+        {
+            // Jika terjadi error
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    // GET BY ID: Region
+    public static void GetRegionById(int id) {
+        // Membuat objek koneksi ke database
+        using var connection = new SqlConnection(connectionString);
+
+        // Membuat objek untuk perintah SQL
+        using var command = new SqlCommand();
+
+        // Mengatur koneksi untuk objek perintah SQL
+        command.Connection = connection;
+
+        // Menentukan query yang akan dijalankan dengan mengambil record berdasarkan id
+        command.CommandText = "SELECT * FROM regions WHERE id=@id;";
+
+        try
+        {
+            // Membuat parameter untuk query SQL
+            var pId = new SqlParameter();
+
+            // Menentukan nama parameter
+            pId.ParameterName = "@id";
+
+            // Menentukan nilai parameter dengan nilai id
+            pId.Value = id;
+
+            // Menetapkan tipe data parameter
+            pId.SqlDbType = SqlDbType.Int;
+
+            // Menambahkan parameter ke objek perintah SQL
+            command.Parameters.Add(pId);
+
+            connection.Open();
+
+            // Mengeksekusi perintah SQL dan mendapatkan hasil dalam bentuk SqlDataReader
+            using var reader = command.ExecuteReader();
+
+            // Memeriksa apakah ada baris data yang ditemukan dan mencetak hasil
+            if (reader.HasRows)
+                while (reader.Read())
+                {
+                    Console.WriteLine("Id: " + reader.GetInt32(0));
+                    Console.WriteLine("Name: " + reader.GetString(1));
+                }
+            else
+                Console.WriteLine("No rows found.");
+
+            reader.Close();
+            connection.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    // INSERT: Region
+    public static void InsertRegion(string name)
+    {
+        using var connection = new SqlConnection(connectionString);
+        using var command = new SqlCommand();
+
+        command.Connection = connection;
+
+        // Menentukan query yang akan dijalankan untuk menambah record dengan field name
+        command.CommandText = "INSERT INTO regions VALUES (@name);";
+
+        try
+        {
+            // Membuat parameter SQL untuk mengganti nilai parameter @name
+            var pName = new SqlParameter();
+            pName.ParameterName = "@name";
+            pName.Value = name;
+            pName.SqlDbType = SqlDbType.VarChar;
+            command.Parameters.Add(pName);
+
+            connection.Open();
+
+            // Memulai transaction
+            using var transaction = connection.BeginTransaction();
+            try
+            {
+                // Menetapkan transaction untuk command
+                command.Transaction = transaction;
+
+                var result = command.ExecuteNonQuery();
+
+                // Melakukan commit transaction jika perintah berhasil
+                transaction.Commit();
+                connection.Close();
+
+                // Memeriksa hasil eksekusi perintah dan memberikan pesan sesuai
+                switch (result)
+                {
+                    case >= 1:
+                        Console.WriteLine("Insert Success");
+                        break;
+                    default:
+                        Console.WriteLine("Insert Failed");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Melakukan rollback transaction jika terjadi kesalahan
+                transaction.Rollback();
+                Console.WriteLine($"Error Transaction: {ex.Message}");
+            }
+        }
+        catch (Exception ex)
+        {
+            // Menangani kesalahan saat membuka koneksi atau eksekusi command
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    // UPDATE: Region
+    public static void UpdateRegion(int id, string name) 
+    {
+        using var connection = new SqlConnection(connectionString);
+        using var command = new SqlCommand();
+
+        command.Connection = connection;
+
+        // Menentukan query yang akan dijalankan untuk update record berdasarkan id
+        command.CommandText = "UPDATE regions SET name = @name WHERE id = @id;";
+
+        try
+        {
+            // Membuat parameter SQL untuk mengganti nilai parameter @id
+            var pId = new SqlParameter();
+            pId.ParameterName = "@id";
+            pId.Value = id;
+            pId.SqlDbType = SqlDbType.Int;
+            command.Parameters.Add(pId);
+
+            // Membuat parameter SQL untuk mengganti nilai parameter @name
+            var pName = new SqlParameter();
+            pName.ParameterName = "@name";
+            pName.Value = name;
+            pName.SqlDbType = SqlDbType.VarChar;
+            command.Parameters.Add(pName);
+
+            connection.Open();
+            using var transaction = connection.BeginTransaction();
+            try
+            {
+                command.Transaction = transaction;
+
+                var result = command.ExecuteNonQuery();
+
+                transaction.Commit();
+                connection.Close();
+
+                // Memeriksa hasil eksekusi command dan memberikan pesan sesuai
+                switch (result)
+                {
+                    case 1:
+                        Console.WriteLine("Update Success");
+                        break;
+                    default:
+                        Console.WriteLine("Update Failed");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Melakukan rollback transaction jika terjadi kesalahan
+                transaction.Rollback();
+                Console.WriteLine($"Error Transaction: {ex.Message}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    // DELETE: Region
+    public static void DeleteRegion(int id) 
+    {
+        using var connection = new SqlConnection(connectionString);
+        using var command = new SqlCommand();
+
+        command.Connection = connection;
+
+        // Menentukan query yang akan dijalankan untuk delete record berdasarkan id
+        command.CommandText = "DELETE FROM regions WHERE id = @id;";
+
+        try
+        {
+            // Membuat parameter SQL untuk mengganti nilai parameter @id
+            var pId = new SqlParameter();
+            pId.ParameterName = "@id";
+            pId.Value = id;
+            pId.SqlDbType = SqlDbType.Int;
+            command.Parameters.Add(pId);
+
+            connection.Open();
+            using var transaction = connection.BeginTransaction();
+            try
+            {
+                command.Transaction = transaction;
+
+                var result = command.ExecuteNonQuery();
+
+                transaction.Commit();
+                connection.Close();
+
+                // Memeriksa hasil eksekusi command dan memberikan pesan sesuai
+                switch (result)
+                {
+                    case >= 1:
+                        Console.WriteLine("Delete Success");
+                        break;
+                    default:
+                        Console.WriteLine("Delete Failed");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Melakukan rollback transaction jika terjadi kesalahan
+                transaction.Rollback();
+                Console.WriteLine($"Error Transaction: {ex.Message}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
---
+++ KesiswaanBasicCSharp/Kesiswaan/Program.cs
@@ -0,0 +1,351 @@
+﻿using System;
+
+public class Siswa
+{
+    protected string nama;
+    protected string kelas;
+    protected string nisn;
+    protected string jenisKelamin;
+
+    // Constructor siswa
+    public Siswa(string nama, string kelas, string nisn, string jenisKelamin)
+    {
+        this.nama = nama;
+        this.kelas = kelas;
+        this.nisn = nisn;
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public string GetNama()
+    {
+        return nama;
+    }
+
+    public string GetKelas()
+    {
+        return kelas;
+    }
+
+    public string GetNISN()
+    {
+        return nisn;
+    }
+
+    public string GetJenisKelamin()
+    {
+        return jenisKelamin;
+    }
+
+    // Menghitung nilai rataan siswa
+    public static float RataanNilai(float nilai1, float nilai2, float nilai3)
+    {
+        return (nilai1 + nilai2 + nilai3) / 3;
+    }
+
+    // Menentukan kelulusan siswa
+    public static string Kelulusan(float rataanNilai)
+    {
+        if (rataanNilai >= 75)
+            return "Lulus";
+        else
+            return "Tidak Lulus";
+    }
+
+    // Print data siswa
+    public virtual void DataSiswa()
+    {
+        Console.WriteLine("Nama : " + nama);
+        Console.WriteLine("Kelas : " + kelas);
+        Console.WriteLine("NISN : " + nisn);
+        Console.WriteLine("Jenis Kelamin : " + jenisKelamin + "\n");
+    }
+}
+
+public class SiswaIPA : Siswa
+{
+    private float NilaiBiologi { get; set; }
+    private float NilaiKimia { get; set; }
+    private float NilaiFisika { get; set; }
+
+    // Constructor siswa IPA
+    public SiswaIPA(string? nama, string? kelas, string? nisn, string? jenisKelamin) : base(nama, kelas, nisn, jenisKelamin)
+    {
+        NilaiBiologi = 0;
+        NilaiKimia = 0;
+        NilaiFisika = 0;
+    }
+
+    // Getter dan setter untuk nilai Biologi
+    public float GetBiologi()
+    {
+        return NilaiBiologi;
+    }
+
+    public void SetBiologi(float nilai)
+    {
+        NilaiBiologi = nilai;
+    }
+
+    // Getter dan setter untuk nilai Fisika
+    public float GetFisika()
+    {
+        return NilaiFisika;
+    }
+
+    public void SetFisika(float nilai)
+    {
+        NilaiFisika = nilai;
+    }
+
+    public float GetKimia()
+    {
+        return NilaiKimia;
+    }
+
+    public void SetKimia(float nilai)
+    {
+        NilaiKimia = nilai;
+    }
+
+    // Print data siswa IPA
+    public override void DataSiswa()
+    {
+        base.DataSiswa();
+        Console.WriteLine($"Nilai Biologi : {NilaiBiologi}");
+        Console.WriteLine($"Nilai Fisika : {NilaiFisika}");
+        Console.WriteLine($"Nilai Kimia : {NilaiKimia}");
+    }
+
+    public void KelulusanSiswaIPA()
+    {
+        base.DataSiswa();
+        Console.WriteLine($"Nilai Rata-rata : {RataanNilai(NilaiBiologi, NilaiKimia, NilaiFisika)}");
+        Console.WriteLine($"Hasil Kelulusan : {Kelulusan(RataanNilai(NilaiBiologi, NilaiKimia, NilaiFisika))}\n");
+    }
+}
+
+public class SiswaIPS : Siswa
+{
+    private float NilaiGeografi { get; set; }
+    private float NilaiSosiologi { get; set; }
+    private float NilaiEkonomi { get; set; }
+
+    //Constructor siswa IPS
+    public SiswaIPS(string nama, string kelas, string nisn, string jenisKelamin) : base(nama, kelas, nisn, jenisKelamin)
+    {
+        NilaiGeografi = 0;
+        NilaiSosiologi = 0;
+        NilaiEkonomi = 0;
+    }
+
+    // Getter dan setter untuk nilai Geografi
+    public float GetGeografi()
+    {
+        return NilaiGeografi;
+    }
+
+    public void SetGeografi(float nilai)
+    {
+        NilaiGeografi = nilai;
+    }
+
+    // Getter dan setter untuk nilai Ekonomi
+    public float GetEkonomi()
+    {
+        return NilaiEkonomi;
+    }
+
+    public void SetEkonomi(float nilai)
+    {
+        NilaiEkonomi = nilai;
+    }
+
+    // Getter dan setter untuk nilai Sosiologi
+    public float GetSosiologi()
+    {
+        return NilaiSosiologi;
+    }
+
+    public void SetSosiologi(float nilai)
+    {
+        NilaiSosiologi = nilai;
+    }
+
+    // Print data siswa IPS
+    public override void DataSiswa()
+    {
+        base.DataSiswa();
+        Console.WriteLine($"Nilai Sosiologi : {NilaiSosiologi}");
+        Console.WriteLine($"Nilai Geografi : {NilaiGeografi}");
+        Console.WriteLine($"Nilai Ekonomi : {NilaiEkonomi}");
+    }
+
+    public void KelulusanSiswaIPS()
+    {
+        base.DataSiswa();
+        Console.WriteLine($"Nilai Rata-rata : {RataanNilai(NilaiSosiologi, NilaiGeografi, NilaiEkonomi)}");
+        Console.WriteLine($"Hasil Kelulusan : {Kelulusan(RataanNilai(NilaiSosiologi, NilaiGeografi, NilaiEkonomi))}\n");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        List<SiswaIPA> daftarSiswaIPA = new();
+        List<SiswaIPS> daftarSiswaIPS = new();
+
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~ SELAMAT DATANG ~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        int opsi;
+        //Tampilan menu
+        do
+        {
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("\nMenu:");
+            Console.WriteLine("1. Input data");
+            Console.WriteLine("2. Lihat nilai siswa");
+            Console.WriteLine("3. Cek kelulusan");
+            Console.WriteLine("4. Keluar");
+            Console.Write("Silahkan pilih opsi (1-4): ");
+
+            opsi = int.Parse(Console.ReadLine());
+            if(opsi == 1 || opsi ==2 || opsi == 3 || opsi == 4)
+                switch (opsi)
+                {
+                    case 1:
+                        Console.WriteLine("\nAnda memilih: Input data");
+                        // Fungsi untuk input data
+                        //Nama
+                        Console.Write("Nama: ");
+                        var nama = Console.ReadLine();
+
+                        //Kelas
+                        Console.Write("Kelas: ");
+                        var kelas = Console.ReadLine();
+
+                        //NISN
+                        Console.Write("NISN: ");
+                        var nisn = Console.ReadLine();
+
+                        //Jenis Kelamin
+                        Console.Write("Jenis Kelamin: ");
+                        var jenisKelamin = Console.ReadLine();
+
+                        //Jurusan
+                        Console.Write("Jurusan (IPA/IPS): ");
+                        var jurusan = Console.ReadLine();
+
+                        //input nilai khusus jurusan
+                        if(jurusan == "IPA")
+                        {
+                            SiswaIPA siswa = new(nama, kelas, nisn, jenisKelamin);
+
+                            Console.Write("Nilai Biologi: ");
+                            float nilaiBiologi = float.Parse(Console.ReadLine());
+                            siswa.SetBiologi(nilaiBiologi);
+
+                            Console.Write("Nilai Fisika: ");
+                            float nilaiFisika = float.Parse(Console.ReadLine());
+                            siswa.SetFisika(nilaiFisika);
+
+                            Console.Write("Nilai Kimia: ");
+                            float nilaiKimia = float.Parse(Console.ReadLine());
+                            siswa.SetKimia(nilaiKimia);
+
+                            daftarSiswaIPA.Add(siswa);
+                        }
+                        else if(jurusan == "IPS")
+                        {
+                            SiswaIPS siswa = new(nama, kelas, nisn, jenisKelamin);
+
+                            Console.Write("Nilai Sosiologi: ");
+                            float nilaiSosiologi = float.Parse(Console.ReadLine());
+                            siswa.SetSosiologi(nilaiSosiologi);
+
+                            Console.Write("Nilai Geografi: ");
+                            float nilaiGeografi = float.Parse(Console.ReadLine());
+                            siswa.SetGeografi(nilaiGeografi);
+
+                            Console.Write("Nilai Ekonomi: ");
+                            float nilaiEkonomi = float.Parse(Console.ReadLine());
+                            siswa.SetEkonomi(nilaiEkonomi);
+
+                            daftarSiswaIPS.Add(siswa);
+                        }
+                        else
+                            Console.Write("Masukan tidak valid.");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("\nAnda memilih: Lihat nilai siswa");
+                        int index = 1;
+
+                        //Menampilkan nilai siswa
+                        if (daftarSiswaIPA.Count > 0 || daftarSiswaIPS.Count > 0)
+                        {
+                            //Menampilkan nilai siswa IPA
+                            foreach (var siswa in daftarSiswaIPA)
+                            {
+                                Console.WriteLine($"~~~~~~~~~~~~~~ Siswa {index} ~~~~~~~~~~~~~~");
+                                siswa.DataSiswa(); // Method print data nilai
+                                index++;
+                            }
+
+                            //Menampilkan nilai siswa IPS
+                            foreach (var siswa in daftarSiswaIPS)
+                            {
+                                Console.WriteLine($"~~~~~~~~~~~~~~ Siswa {index} ~~~~~~~~~~~~~~");
+                                siswa.DataSiswa(); // Method print data nilai
+                                index++;
+                            }
+                        }
+                        else
+                            Console.WriteLine("Tidak ada data siswa.");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("\nAnda memilih: Cek kelulusan");
+
+                        int id = 1;
+                        //Menampilkan nilai siswa
+                        if (daftarSiswaIPA.Count > 0 || daftarSiswaIPS.Count > 0)
+                        {
+                            //Menampilkan nilai siswa IPA
+                            foreach (var siswa in daftarSiswaIPA)
+                            {
+                                Console.WriteLine($"~~~~~~~~~~~~~~ Siswa {id} ~~~~~~~~~~~~~~");
+                                siswa.KelulusanSiswaIPA();
+                                id++;
+                            }
+
+                            //Menampilkan nilai siswa IPS
+                            foreach (var siswa in daftarSiswaIPS)
+                            {
+                                Console.WriteLine($"~~~~~~~~~~~~~~ Siswa {id} ~~~~~~~~~~~~~~");
+                                siswa.KelulusanSiswaIPS();
+                                id++;
+                            }
+                        }
+                        else
+                            Console.WriteLine("Tidak ada data siswa.");
+                        break;
+
+                    case 4:
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        Console.WriteLine("~~~~~~ TERIMA KASIH SUDAH MENGUNJUNGI KAMI, SAMPAI JUMPA KEMBALI ~~~~~~");
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opsi tidak valid. Silakan pilih kembali.");
+                        break;
+                }
+            else
+            {
+                Console.WriteLine("Masukan tidak valid. Silakan masukkan angka (1-4).");
+            }
+        } while (opsi != 4);
+    }
+}
