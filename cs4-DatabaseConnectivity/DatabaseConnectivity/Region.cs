00db4da7ed5ee79182d210782360625e9fbf3e1b
Aryo Aditya <aryoaditya2000@gmail.com>
2023-09-20 14:56:49 +0700

    update region class

---
+++ cs4-DatabaseConnectivity/DatabaseConnectivity/Region.cs
@@ -0,0 +1,277 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace DatabaseConnectivity
+{
+    public class Region
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        private readonly string connectionString = "Data Source=DESKTOP-98R3UR4;Database = db_hr_dts; Integrated Security=True;Connect Timeout=30;";
+
+        // GET ALL: Region
+        public List<Region> GetAll()
+        {
+            var regions = new List<Region>();
+
+            using var connection = new SqlConnection(connectionString); // Membuat objek koneksi ke database
+            using var command = new SqlCommand(); // Membuat objek untuk perintah SQL
+
+            command.Connection = connection; // Mengatur koneksi untuk objek perintah SQL
+            command.CommandText = "SELECT * FROM regions"; // Query SELECT yang akan dijalankan
+
+            try
+            {                
+                connection.Open(); // Membuka koneksi ke database
+
+                using var reader = command.ExecuteReader(); // Mengeksekusi perintah SQL dan mendapatkan objek reader
+
+                // Memeriksa apakah ada baris data yang ditemukan
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        regions.Add(new Region
+                        {
+                            Id = reader.GetInt32(0),
+                            Name = reader.GetString(1)
+                        });
+                    }
+                    reader.Close();
+                    connection.Close();
+
+                    return regions;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Jika terjadi error
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            return new List<Region>();
+        }
+
+        // GET BY ID: Region
+        public Region? GetById(int id)
+        {            
+            using var connection = new SqlConnection(connectionString); // Membuat objek koneksi ke database
+            using var command = new SqlCommand(); // Membuat objek untuk perintah SQL
+
+            command.Connection = connection; // Mengatur koneksi untuk objek perintah SQL
+            command.CommandText = "SELECT * FROM regions WHERE id=@id;"; // Query yang akan dijalankan
+
+            try
+            {
+                // Membuat parameter untuk query SQL
+                var pId = new SqlParameter
+                {
+                    ParameterName = "@id",
+                    Value = id,
+                    SqlDbType = SqlDbType.Int
+                };
+                command.Parameters.Add(pId);
+
+                connection.Open();
+
+                using var reader = command.ExecuteReader(); // Mengeksekusi perintah SQL dan mendapatkan objek reader
+                
+                // Memeriksa apakah ada baris data yang ditemukan dan mencetak hasil
+                if (reader.HasRows)
+                {
+                    reader.Read();
+
+                    Region reg = new Region
+                    {
+                        Id = reader.GetInt32(0),
+                        Name = reader.GetString(1)
+                    };
+
+                    reader.Close();
+                    connection.Close();
+
+                    return reg;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            return null;
+        }
+
+        // INSERT: Region
+        public string Insert(string name)
+        {
+            using var connection = new SqlConnection(connectionString);
+            using var command = new SqlCommand();
+
+            command.Connection = connection;
+            command.CommandText = "INSERT INTO regions VALUES (@name);"; // Query yang akan dijalankan
+
+            try
+            {
+                // Membuat parameter SQL untuk mengganti nilai parameter @name
+                var pName = new SqlParameter
+                {
+                    ParameterName = "@name",
+                    Value = name,
+                    SqlDbType = SqlDbType.VarChar
+                };
+                command.Parameters.Add(pName);
+
+                connection.Open();
+
+                // Memulai transaction
+                using var transaction = connection.BeginTransaction();
+                try
+                {
+                    // Menetapkan transaction untuk command
+                    command.Transaction = transaction;
+
+                    var result = command.ExecuteNonQuery();
+
+                    // Melakukan commit transaction jika perintah berhasil
+                    transaction.Commit();
+                    connection.Close();
+
+                    return result.ToString();
+                }
+                catch (Exception ex)
+                {
+                    // Melakukan rollback transaction jika terjadi kesalahan
+                    transaction.Rollback();
+                    return $"Error Transaction: {ex.Message}";
+                }
+            }
+            catch (Exception ex)
+            {
+                // Menangani kesalahan saat membuka koneksi atau eksekusi command
+                return $"Error: {ex.Message}";
+            }
+        }
+
+        // UPDATE: Region
+        public string Update(int id, string name)
+        {
+            using var connection = new SqlConnection(connectionString);
+            using var command = new SqlCommand();
+            string temp;
+
+            command.Connection = connection;
+
+            // Menentukan query yang akan dijalankan untuk update record berdasarkan id
+            command.CommandText = "UPDATE regions SET name = @name WHERE id = @id;";
+
+            try
+            {
+                // Membuat parameter SQL untuk mengganti nilai parameter @id
+                var pId = new SqlParameter
+                {
+                    ParameterName = "@id",
+                    Value = id,
+                    SqlDbType = SqlDbType.Int
+                };
+                command.Parameters.Add(pId);
+
+                // Membuat parameter SQL untuk mengganti nilai parameter @name
+                var pName = new SqlParameter
+                {
+                    ParameterName = "@name",
+                    Value = name,
+                    SqlDbType = SqlDbType.VarChar
+                };
+                command.Parameters.Add(pName);
+
+                connection.Open();
+                using var transaction = connection.BeginTransaction();
+                try
+                {
+                    command.Transaction = transaction;
+
+                    var result = command.ExecuteNonQuery();
+
+                    transaction.Commit();
+                    connection.Close();
+
+                    // Memeriksa hasil eksekusi command dan memberikan pesan sesuai
+                    temp = result switch
+                    {
+                        >= 1 => "Update Success",
+                        _ => "Update Failed",
+                    };
+                    return temp;
+                }
+                catch (Exception ex)
+                {
+                    // Melakukan rollback transaction jika terjadi kesalahan
+                    transaction.Rollback();
+                    return $"Error Transaction: {ex.Message}";
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"Error: {ex.Message}";
+            }
+
+            
+        }
+
+        // DELETE: Region
+        public string Delete(int id)
+        {
+            using var connection = new SqlConnection(connectionString);
+            using var command = new SqlCommand();
+            string temp;
+
+            command.Connection = connection;
+
+            // Menentukan query yang akan dijalankan untuk delete record berdasarkan id
+            command.CommandText = "DELETE FROM regions WHERE id = @id;";
+
+            try
+            {
+                // Membuat parameter SQL untuk mengganti nilai parameter @id
+                var pId = new SqlParameter
+                {
+                    ParameterName = "@id",
+                    Value = id,
+                    SqlDbType = SqlDbType.Int
+                };
+                command.Parameters.Add(pId);
+
+                connection.Open();
+                using var transaction = connection.BeginTransaction();
+                try
+                {
+                    command.Transaction = transaction;
+
+                    var result = command.ExecuteNonQuery();
+
+                    transaction.Commit();
+                    connection.Close();
+
+                    // Memeriksa hasil eksekusi command dan memberikan pesan sesuai
+                    temp = result switch
+                    {
+                        >= 1 => "Delete Success",
+                        _ => "Delete Failed",
+                    };
+                    return temp;
+                }
+                catch (Exception ex)
+                {
+                    // Melakukan rollback transaction jika terjadi kesalahan
+                    transaction.Rollback();
+                    return $"Error Transaction: {ex.Message}";
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"Error: {ex.Message}";
+            }
+        }
+    }
+}
---
+++ cs4-DatabaseConnectivity/DatabaseConnectivity/Show.cs
@@ -0,0 +1,69 @@
+﻿namespace DatabaseConnectivity
+{
+    public class RegionUI
+    {
+        // Cetak hasil All Region
+        public static void AllRegion(Region region)
+        {
+            var getAllRegion = region.GetAll();
+
+            if (getAllRegion.Count > 0)
+            {
+                foreach (var reg in getAllRegion)
+                {
+                    Console.WriteLine($"Id: {reg.Id}, Name: {reg.Name}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No data found");
+            }
+        }
+
+        // Cetak hasil Region by Id
+        public static void RegionById(Region region, int id)
+        {
+            var getRegionById = region.GetById(id);
+            if (getRegionById != null)
+            {
+                Console.WriteLine("Hasil yang ditemukan : ");
+                Console.WriteLine($"Id: {getRegionById.Id}, Name: {getRegionById.Name}");
+            }
+            else
+                Console.WriteLine("Hasil tidak ditemukan");
+            
+        }
+
+        // Cetak hasil Insert Region
+        public static void InsertRegion(Region region)
+        {
+            var insertResult = region.Insert("New Region");
+            int.TryParse(insertResult, out int result);
+            if (result > 0)
+            {
+                Console.WriteLine("Insert Success");
+            }
+            else
+            {
+                Console.WriteLine("Insert Failed");
+                Console.WriteLine(insertResult);
+            }
+        }
+
+        // Cetak hasil Update Region
+        public static void UpdateRegion(Region region, int id, string nama)
+        {
+            var updateResult = region.Update(id, nama);
+
+            Console.WriteLine(updateResult);
+        }
+
+        // Cetak hasil Delete Region
+        public static void DeleteRegion(Region region, int id)
+        {
+            var deleteResult = region.Delete(id);
+
+            Console.WriteLine(deleteResult);
+        }
+    }
+}
